223155f01292407b499cb4895d17fd00fe9ae2fe
Jackson Ramos <jackramos021495@gmail.com>
2020-05-04 20:33:30 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+  integer -= 1
+  end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+  while integer >= 5
+    puts "should take at least 5 seconds to execute"
+    sleep(integer)
+    integer -= 1
+
+
+  end
 end
